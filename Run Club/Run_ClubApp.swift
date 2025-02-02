2e546a3ae0fe6eddf5a0971254bfaf0b3d6276d1
Karen Tran <karentran28@hotmail.com>
2025-02-02 11:51:57 -0800

    Initial Commit

---
+++ Run Club/Run_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_ClubApp.swift
+//  Run Club
+//
+//  Created by Karen Tran on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct Run_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
