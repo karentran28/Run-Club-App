f037b4ff418d9c1f4542d6c58156306429f1ef6d
Karen Tran <karentran28@hotmail.com>
2025-02-06 22:57:30 -0800

    Added RunView

--- Run Club/RunView.swift
+++ Run Club/RunView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct RunView: View {
+    @EnvironmentObject var runTracker: RunTracker
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack {
+                    Text("Distance")
+                }
+                .frame(maxWidth: .infinity)
+                
+                VStack {
+                    Text("BPM")
+                }
+                .frame(maxWidth: .infinity)
+                
+                VStack {
+                    Text("Pace")
+                }
+                .frame(maxWidth: .infinity)
+            }
+            
+            VStack {
+                Text("00:00")
+                    .font(.system(size: 64))
+                
+                Text("Time")
+                    .foregroundStyle(.gray)
+            }
+            .frame(maxHeight: .infinity)
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "stop.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .padding(36)
+                        .background(.black)
+                        .clipShape(Circle())
+                }
+                .frame(maxWidth: .infinity)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "pause.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .padding(36)
+                        .background(.black)
+                        .clipShape(Circle())
+                }
+                .frame(maxWidth: .infinity)
+            }
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background(.yellow)
     }
 }
 
